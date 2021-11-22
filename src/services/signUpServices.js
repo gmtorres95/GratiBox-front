a9e1afc5389eddc4d08a46358ef2e52a704b2758
Gabriel Torres <al2012.163.torres@gmail.com>
2021-11-21 21:22:41 -0300

    fix: communication with heroku

--- src/services/signUpServices.js
+++ src/services/signUpServices.js
@@ -4,7 +4,7 @@ import sendAlert from '../helpers/sendAlert';
 const URL = process.env.REACT_APP_API_BASE_URL;
 
 export function createNewUser(body, setIsButtonEnabled, navigate) {
-  axios.post(URL+'/sign-up', body)
+  axios.post(URL+'sign-up', body)
     .then((resp) => navigate('/sign-in'))
     .catch((err) => {
       if(err.response.status === 409) sendAlert('Erro no cadastro!','Email já cadastrado','error');
